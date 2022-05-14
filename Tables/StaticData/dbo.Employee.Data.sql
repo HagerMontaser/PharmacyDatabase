836820c42ed303ad47c4752d62846a11a3948052
Doha <dohaeshra96@gmail.com>
2022-05-14 18:28:59 +0200

    Doha's first commit

--- Tables/StaticData/dbo.Employee.Data.sql
+++ Tables/StaticData/dbo.Employee.Data.sql
@@ -15,4 +15,5 @@ INSERT dbo.Employee(UserName, Password, Name, Role) VALUES (N'rehab_tany', N'123
 
 INSERT dbo.Employee(UserName, Password, Name, Role) VALUES (N'Rowann', N'123', NULL, N'user');
 
-INSERT dbo.Employee(UserName, Password, Name, Role) VALUES (N'marian', N'1234', NULL, N'user');
+
+INSERT dbo.Employee(UserName, Password, Name, Role) VALUES (N'Ashrakat', N'1234', NULL, N'user');
