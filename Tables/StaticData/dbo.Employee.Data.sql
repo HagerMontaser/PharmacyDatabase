9f0d95679e31160a696036ddd72978de97891a7c
Rehab Samy <rehabsamy18@gmail.com>
2022-05-14 16:02:12 +0200

    Rehab Second Trial

--- Tables/StaticData/dbo.Employee.Data.sql
+++ Tables/StaticData/dbo.Employee.Data.sql
@@ -10,3 +10,5 @@ INSERT dbo.Employee(UserName, Password, Name, Role) VALUES (N'rowan', N'1234', N
 INSERT dbo.Employee(UserName, Password, Name, Role) VALUES (N'sokr', N'1234', NULL, N'user');
 
 INSERT dbo.Employee(UserName, Password, Name, Role) VALUES (N'rehab_betgarab', N'1234', NULL, N'user');
+
+INSERT dbo.Employee(UserName, Password, Name, Role) VALUES (N'rehab_tany', N'1234', NULL, N'user');
