8994ed6f493c7c49d9d9e7c77cfdde6c1420faf5
= <=>
2022-05-25 12:01:46 +0200

    add authentication

--- Tables/dbo.Employee.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Employee] (
-  [UserName] [nvarchar](450) NOT NULL,
-  [Password] [nchar](50) NOT NULL,
-  [Name] [nchar](20) NULL,
-  [Role] [nchar](10) NOT NULL,
-  CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([UserName])
-)
-ON [PRIMARY]
-GO
