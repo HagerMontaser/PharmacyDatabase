fb83e9801fdbfb7295b28a604f7be0cb8ee88c0f
hagermontaser5 <hagermontaser5@gmail.com>
2022-05-27 17:17:59 +0200

    data updated

--- Tables/StaticData/dbo.Invoice.Data.sql
+++ Tables/StaticData/dbo.Invoice.Data.sql
@@ -1,6 +1,6 @@
 ﻿SET IDENTITY_INSERT dbo.Invoice ON
 GO
-INSERT dbo.Invoice(Invoice_No, Date, CustName, TotalPrice, Type, Employee_Username) VALUES (1, '2022-05-13', N'malk', NULL, N'sell', N'hager');
+
 GO
 SET IDENTITY_INSERT dbo.Invoice OFF
 GO
