281a4b28538c6bbc4f27160f3fb070b6b0172f7b
hagermontaser5 <hagermontaser5@gmail.com>
2022-05-27 17:19:14 +0200

    data updated

---
+++ Tables/StaticData/dbo.AspNetRoles.Data.sql
@@ -0,0 +1,2 @@
+﻿INSERT dbo.AspNetRoles(Id, Name, NormalizedName, ConcurrencyStamp) VALUES (N'1', N'Admin', N'Admin', NULL);
+INSERT dbo.AspNetRoles(Id, Name, NormalizedName, ConcurrencyStamp) VALUES (N'2', N'Employee', N'Employee', NULL);
---
+++ Tables/StaticData/dbo.AspNetUserRoles.Data.sql
@@ -0,0 +1,1 @@
+﻿INSERT dbo.AspNetUserRoles(UserId, RoleId) VALUES (N'ee224e23-42fa-48e9-bb9d-e40547a2347e', N'1');
